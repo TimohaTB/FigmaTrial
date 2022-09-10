ebaaf66ff8a19b084b22f8dbd33ac48f430271e4
timoha <timoha@timoha.com>
2022-09-10 20:00:36 +0300

    initial commitment

---
+++ app/src/main/java/exportkit/figma/android_small___5_activity.java
@@ -0,0 +1,49 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_small___2
+	 *	@date 		Saturday 10th of September 2022 04:35:18 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class android_small___5_activity extends Activity {
+
+	
+	private View _bg__android_small___5_ek2;
+	private ImageView img_0961_2_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_small___5);
+
+		
+		_bg__android_small___5_ek2 = (View) findViewById(R.id._bg__android_small___5_ek2);
+		img_0961_2_ek1 = (ImageView) findViewById(R.id.img_0961_2_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
